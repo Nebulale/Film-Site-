bb6963d08093275e96fc43e70a831c6e83f0180f
Nebulale <93530318+Nebulale@users.noreply.github.com>
2021-03-31 08:30:16 +0000

    update translation template

--- app/src/androidTest/java/com/tutorial/stockmarketapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/tutorial/stockmarketapp/ExampleInstrumentedTest.kt
@@ -19,4 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.tutorial.stockmarketapp", appCon
+        assertEquals("com.tutorial.stockmarketapp", appContext.packageName)
+    }
+}
