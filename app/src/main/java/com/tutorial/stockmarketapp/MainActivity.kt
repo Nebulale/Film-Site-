c9ed252d4ba1d6da32f1e66a5df31b4e543d6c5c
Nebulale <93530318+Nebulale@users.noreply.github.com>
2021-04-08 16:49:17 +0000

    Updated manifest version

--- app/src/main/java/com/tutorial/stockmarketapp/MainActivity.kt
+++ app/src/main/java/com/tutorial/stockmarketapp/MainActivity.kt
@@ -3,4 +3,5 @@ package com.tutorial.stockmarketapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.lay
+import androidx.compose.foundation.layout.fillMaxSize
+impo
