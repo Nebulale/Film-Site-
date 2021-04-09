4e1034e857221df42e925d6ae4c285912c07a21f
Nebulale <93530318+Nebulale@users.noreply.github.com>
2021-04-09 05:24:51 +0000

    fix upload

--- app/src/main/java/com/tutorial/stockmarketapp/MainActivity.kt
+++ app/src/main/java/com/tutorial/stockmarketapp/MainActivity.kt
@@ -4,4 +4,4 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-impo
+import androidx.compos
