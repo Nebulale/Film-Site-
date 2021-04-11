cbdc67ed2c16c6ccf1773b8ba07065f5d58812f1
Nebulale <93530318+Nebulale@users.noreply.github.com>
2021-04-11 05:42:34 +0000

    usb: cp210x new Vendor/Device IDs

--- app/src/main/java/com/tutorial/stockmarketapp/MainActivity.kt
+++ app/src/main/java/com/tutorial/stockmarketapp/MainActivity.kt
@@ -4,4 +4,5 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialT
+import androidx.compose.material.MaterialTheme
+import androidx.com
