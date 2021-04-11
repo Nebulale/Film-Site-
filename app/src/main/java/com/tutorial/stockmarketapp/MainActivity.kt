e5301e57f87160de5b836db034af751a8eacd09f
Nebulale <93530318+Nebulale@users.noreply.github.com>
2021-04-11 20:42:01 +0000

    Readme angepasst

--- app/src/main/java/com/tutorial/stockmarketapp/MainActivity.kt
+++ app/src/main/java/com/tutorial/stockmarketapp/MainActivity.kt
@@ -5,4 +5,4 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
-import androidx.com
+import androidx.compose.material.Sur
