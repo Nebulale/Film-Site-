15bd041bdb39a732067c317f8e05252ad765dd6e
Nebulale <93530318+Nebulale@users.noreply.github.com>
2021-04-17 08:56:10 +0000

    lets try again

--- app/src/main/java/com/tutorial/stockmarketapp/MainActivity.kt
+++ app/src/main/java/com/tutorial/stockmarketapp/MainActivity.kt
@@ -7,4 +7,4 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
-impor
+import com.tutorial.stockmar
