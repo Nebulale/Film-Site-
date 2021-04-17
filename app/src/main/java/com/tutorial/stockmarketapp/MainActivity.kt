9a4629d9d9a7c25b30c2a740b2564f11b7a95715
Nebulale <93530318+Nebulale@users.noreply.github.com>
2021-04-17 05:58:05 +0000

    fix database

--- app/src/main/java/com/tutorial/stockmarketapp/MainActivity.kt
+++ app/src/main/java/com/tutorial/stockmarketapp/MainActivity.kt
@@ -6,4 +6,5 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.
+import androidx.compose.ui.Modifier
+impor
