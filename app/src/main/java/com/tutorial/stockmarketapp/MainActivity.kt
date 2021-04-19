0eae2b6b52d10068bb17dd08eff13eee9d57ee86
Nebulale <93530318+Nebulale@users.noreply.github.com>
2021-04-19 09:37:08 +0000

    removed extra space

--- app/src/main/java/com/tutorial/stockmarketapp/MainActivity.kt
+++ app/src/main/java/com/tutorial/stockmarketapp/MainActivity.kt
@@ -7,4 +7,4 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
-import com.tutorial.stockmarketapp.ui.theme.
+import com.tutorial.stockmarketapp.ui.theme.StockMarketAp
