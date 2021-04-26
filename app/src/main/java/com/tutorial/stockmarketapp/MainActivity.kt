6051177bd21a34936c3764bccd109bb5a61b4509
Nebulale <93530318+Nebulale@users.noreply.github.com>
2021-04-26 17:05:29 +0000

    bootstrapped

--- app/src/main/java/com/tutorial/stockmarketapp/MainActivity.kt
+++ app/src/main/java/com/tutorial/stockmarketapp/MainActivity.kt
@@ -13,4 +13,5 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Stoc
+            StockMarketAppTheme {
+       
