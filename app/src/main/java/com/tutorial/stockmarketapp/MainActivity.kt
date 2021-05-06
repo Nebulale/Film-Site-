280867ed95bc10354ae473645aaa64fc57afcbd6
Nebulale <93530318+Nebulale@users.noreply.github.com>
2021-05-06 11:13:27 +0000

    blog-delploy UPDATE

--- app/src/main/java/com/tutorial/stockmarketapp/MainActivity.kt
+++ app/src/main/java/com/tutorial/stockmarketapp/MainActivity.kt
@@ -17,4 +17,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
-                )
+                ) {
+
+                }
+     
