74bcfa772b40ad5ce570013760b6051ad00bd1da
Nebulale <93530318+Nebulale@users.noreply.github.com>
2021-05-09 15:51:17 +0000

    ssss

--- app/src/main/java/com/tutorial/stockmarketapp/ui/theme/Color.kt
+++ app/src/main/java/com/tutorial/stockmarketapp/ui/theme/Color.kt
@@ -3,4 +3,5 @@ package com.tutorial.stockmarketapp.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF62
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3
