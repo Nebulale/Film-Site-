736c80c973facad46c0c7d656ed2b9f1d71f4087
Nebulale <93530318+Nebulale@users.noreply.github.com>
2021-05-11 09:10:13 +0000

    fix download

--- app/src/main/java/com/tutorial/stockmarketapp/ui/theme/Theme.kt
+++ app/src/main/java/com/tutorial/stockmarketapp/ui/theme/Theme.kt
@@ -25,3 +25,24 @@ private val LightColorPalette = lightColors(
     onSecondary = Color.Black,
     onBackground = Color.Black,
     onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun StockMarketAppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
