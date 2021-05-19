916957a8b8b2e72f736d3723bbe6d091482f04df
Nebulale <93530318+Nebulale@users.noreply.github.com>
2021-05-19 08:20:11 +0000

    Update jshintrc

--- app/src/main/java/com/tutorial/stockmarketapp/ui/theme/Type.kt
+++ app/src/main/java/com/tutorial/stockmarketapp/ui/theme/Type.kt
@@ -2,4 +2,4 @@ package com.tutorial.stockmarketapp.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.comp
+import androidx.compose.ui.text.font.FontFamil
