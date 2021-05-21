43029b97b1283f9014cfc97d6e4c735e41c49e40
Nebulale <93530318+Nebulale@users.noreply.github.com>
2021-05-21 18:02:36 +0000

    docs(CHANGELOG): release

--- app/src/main/java/com/tutorial/stockmarketapp/ui/theme/Type.kt
+++ app/src/main/java/com/tutorial/stockmarketapp/ui/theme/Type.kt
@@ -3,4 +3,4 @@ package com.tutorial.stockmarketapp.ui.theme
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
-import androidx.com
+import androidx.compose.ui.text.font.Fon
