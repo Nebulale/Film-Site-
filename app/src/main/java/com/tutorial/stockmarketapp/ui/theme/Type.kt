6b4ab9c26a2cbb58fec6e1a5879b1ac7998be4ab
Nebulale <93530318+Nebulale@users.noreply.github.com>
2021-05-21 09:33:21 +0000

    Fix Travis CI config

--- app/src/main/java/com/tutorial/stockmarketapp/ui/theme/Type.kt
+++ app/src/main/java/com/tutorial/stockmarketapp/ui/theme/Type.kt
@@ -2,4 +2,5 @@ package com.tutorial.stockmarketapp.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamil
+import androidx.compose.ui.text.font.FontFamily
+import androidx.com
