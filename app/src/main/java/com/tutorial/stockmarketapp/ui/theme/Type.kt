bb93466cf25025637351fda424ec7e668e5e66b6
Nebulale <93530318+Nebulale@users.noreply.github.com>
2021-05-23 15:52:21 +0000

    phpcbf

--- app/src/main/java/com/tutorial/stockmarketapp/ui/theme/Type.kt
+++ app/src/main/java/com/tutorial/stockmarketapp/ui/theme/Type.kt
@@ -4,4 +4,6 @@ import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.u
+import androidx.compose.ui.unit.sp
+
+// Set o
