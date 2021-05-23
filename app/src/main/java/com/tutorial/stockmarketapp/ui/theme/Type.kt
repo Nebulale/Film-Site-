ebb22b630df44465dd71d578e9a6167b044ab838
Nebulale <93530318+Nebulale@users.noreply.github.com>
2021-05-23 06:43:26 +0000

    - bump version for autobuild

--- app/src/main/java/com/tutorial/stockmarketapp/ui/theme/Type.kt
+++ app/src/main/java/com/tutorial/stockmarketapp/ui/theme/Type.kt
@@ -3,4 +3,5 @@ package com.tutorial.stockmarketapp.ui.theme
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.Fon
+import androidx.compose.ui.text.font.FontWeight
+import android
